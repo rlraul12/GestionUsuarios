4793758aaeae477cd3282af01d6161c69889c3c0
Raul E. Paneque <49737084+rlraul12@users.noreply.github.com>
2022-05-25 14:06:06 -0400

    Initial commit

---
+++ src/main/java/com/mycompany/gestionusuarios/GestionUsuarios.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gestionusuarios;
+
+/**
+ *
+ * @author raulpaneque
+ */
+public class GestionUsuarios {
+
+    public static void main(String[] args) {
+   
+        
+        Formulario form = new Formulario();
+        form.show();
+    }
+}
